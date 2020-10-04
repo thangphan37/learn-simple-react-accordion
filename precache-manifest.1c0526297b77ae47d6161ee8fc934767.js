1fcc682627f7112ae2f41917f9cdda204c0296b4
thangphan37 <pcthangk59@gmail.com>
2020-10-05 07:35:39 +0900

    Updates

---
+++ precache-manifest.1c0526297b77ae47d6161ee8fc934767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2eddb7b74d199e597b9eaef73e8e66",
+    "url": "/learn-simple-react-accordion/index.html"
+  },
+  {
+    "revision": "061373ed5a241b70d1ac",
+    "url": "/learn-simple-react-accordion/static/css/main.a46ce108.chunk.css"
+  },
+  {
+    "revision": "f4803ebe711b77c7efd7",
+    "url": "/learn-simple-react-accordion/static/js/2.437d3754.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/learn-simple-react-accordion/static/js/2.437d3754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061373ed5a241b70d1ac",
+    "url": "/learn-simple-react-accordion/static/js/main.0d4401a5.chunk.js"
+  },
+  {
+    "revision": "f015da7521838ffc03a5",
+    "url": "/learn-simple-react-accordion/static/js/runtime-main.ca7c5305.js"
+  }
+]);
